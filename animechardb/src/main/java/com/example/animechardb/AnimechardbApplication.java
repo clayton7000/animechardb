5e6293b465bf49cefaa8154f3ea33537532d114b
clayton7000 <80071108+clayton7000@users.noreply.github.com>
2024-10-27 20:11:01 -0300

    criado classe model personagem

---
+++ animechardb/src/main/java/com/example/animechardb/AnimechardbApplication.java
@@ -0,0 +1,13 @@
+package com.example.animechardb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimechardbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimechardbApplication.class, args);
+	}
+
+}
---
+++ animechardb/src/test/java/com/example/animechardb/AnimechardbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animechardb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimechardbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
